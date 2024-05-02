4abea8a00768b787a0f5da1fffa0b5edfe64955a
az4aaz_ <lucienherve@hotmail.com>
2024-05-02 09:25:03 +0200

    better lights

--- weather/constants.js
+++ weather/constants.js
@@ -1,72 +1,102 @@
 export class Utils {
-  /**
-   * Contains constants used in the rain animation.
-   */
-  static CONSTANTS = {
-    CANVAS_ID: "rain",
-    MIN_LINE_LENGTH: 3,
-    MAX_LINE_LENGTH: 6,
-    MIN_WIDTH: 2,
-    MAX_WIDTH: 5,
-    MIN_SPLASHES: 1,
-    MAX_SPLASHES: 5,
-    MIN_SPEED: 15,
-    MAX_SPEED: 30,
-    MAX_SPLASH_SIZE: 5,
-    MOUSE_RADIUS: 45,
-    RAIN_STEPS: 10,
-    RAINDROP_COLOR: "rgba(255, 255, 255)",
-    SPLASH_DURATION: 100,
-    GROUND_LEVEL: 10,
-    RESOLUTION: 2.5,
-    GRAVITY: 0.3,
-  };
+    /**
+     * Contains constants used in the rain animation.
+     */
+    static CONSTANTS = {
+        CANVAS_ID: "rain",
+        MIN_LINE_LENGTH: 2,
+        MAX_LINE_LENGTH: 4,
+        MIN_WIDTH: 2,
+        MAX_WIDTH: 5,
+        MIN_SPLASHES: 1,
+        MAX_SPLASHES: 5,
+        MIN_SPEED: 20,
+        MAX_SPEED: 90,
+        MAX_SPLASH_SIZE: 5,
+        MOUSE_RADIUS: 45,
+        RAIN_STEPS: 10,
+        RAINDROP_COLOR: "rgba(255, 255, 255, 1)",
+        SPLASH_DURATION: 100,
+        GROUND_LEVEL: 5,
+        RESOLUTION: 5,
+        GRAVITY: 0.3,
+    };
 
-  /**
-   * Generates a random number between min and max.
-   *
-   * @param { number } min
-   * @param { number } max
-   * @returns { number }
-   */
-  static randomBetween(min, max) {
-    return Math.random() * (max - min) + min;
-  }
+    /**
+     * Generates a random number between min and max.
+     *
+     * @param { number } min
+     * @param { number } max
+     * @returns { number }
+     */
+    static randomBetween(min, max) {
+        return Math.random() * (max - min) + min;
+    }
 
-  /**
-   * Calculates the speed of the raindrop based on the resolution.
-   * The speed is inversely proportional to the resolution.
-   *
-   * @param { number } resolution
-   * @returns { number }
-   */
-  static calculateSpeed(resolution) {
-    let randomSpeed = this.randomBetween(
-      this.CONSTANTS.MIN_SPEED,
-      this.CONSTANTS.MAX_SPEED
-    );
-    return randomSpeed / resolution;
-  }
+    /**
+     * Calculates the speed of the raindrop based on the resolution.
+     * The speed is inversely proportional to the resolution.
+     *
+     * @param { number } resolution
+     * @returns { number }
+     */
+    static calculateSpeed(resolution) {
+        let randomSpeed = this.randomBetween(
+            this.CONSTANTS.MIN_SPEED,
+            this.CONSTANTS.MAX_SPEED
+        );
+        return randomSpeed / resolution;
+    }
 
-  /**
-   * Calculates the splash size based on the resolution.
-   * The splash size is directly proportional to the resolution.
-   *
-   * @param { number } resolution
-   * @returns { number }
-   */
-  static calculateSplashSize(resolution) {
-    return resolution / 2;
-  }
+    /**
+     * Calculates the splash size based on the resolution.
+     * The splash size is directly proportional to the resolution.
+     *
+     * @param { number } resolution
+     * @returns { number }
+     */
+    static calculateSplashSize(resolution) {
+        return resolution / 2;
+    }
 
-  /**
-   * Calculates the mouse radius based on the resolution.
-   * The mouse radius is directly proportional to the resolution.
-   *
-   * @param { number } resolution
-   * @returns { number }
-   */
-  static calculateMouseRadius(resolution) {
-    return resolution * 15;
-  }
+    /**
+     * Calculates the mouse radius based on the resolution.
+     * The mouse radius is directly proportional to the resolution.
+     *
+     * @param { number } resolution
+     * @returns { number }
+     */
+    static calculateMouseRadius(resolution) {
+        return resolution * 15;
+    }
+
+    /**
+     * Dims the color based on the distance from the light source.
+     * The closer the object is to the light source, the brighter it is.
+     * The farther the object is from the light source, the dimmer it is.
+     *
+     * @param { string } color  The color to dim.
+     * @param { number } distance The distance from the light source.
+     * @param { number } resolution The resolution of the grid.
+     * @returns { string }
+     */
+    static dimColor(color, distance, resolution) {
+        let alpha = Math.max(0.1, Math.min(1, 1 - distance / resolution));
+        return color.replace(
+            /rgba\((\d+), (\d+), (\d+), (\d+)\)/,
+            `rgba($1, $2, $3, ${alpha})`
+        );
+    }
+
+    /**
+     * Lerp function.
+     * Linear interpolation between two values.
+     * @param { number } a The first value.
+     * @param { number } b The second value.
+     * @param { number } t The interpolation factor.
+     * @returns { number }
+     */
+    static lerp(a, b, t) {
+        return (1 - t) * a + t * b;
+    }
 }
--- weather/rain/controller.js
+++ weather/rain/controller.js
@@ -1,17 +1,22 @@
 import { Raindrop } from "./raindrop.js";
 import { Splash } from "./splash.js";
 import { Utils } from "../constants.js";
+import { LightSource } from "../light.js";
 
 export class RainController {
   /**
    * Generating and moving raindrops and splashes.
    *
-   * @param { NeoPixelGrid } grid
-   * @param { { x: number, y: number } } mouse
+   * @param { NeoPixelGrid } grid   The grid to draw the raindrops and splashes on.
+   * @param { { x: number, y: number } } mouse  The mouse position.
+   * @param { LightSource } light  The light source.
+   * @property { Raindrop[] } raindrops  The raindrops to be generated and moved.
+   * @property { Splash[] } splashes The splashes to be generated and moved.
    */
-  constructor(grid, mouse) {
+  constructor(grid, mouse, light) {
     this.grid = grid;
     this.mouse = mouse;
+    this.light = light;
     this.raindrops = [];
     this.splashes = [];
 
@@ -24,16 +29,16 @@ export class RainController {
    * Generates a raindrop and pushes it to the raindrops array.
    */
   generateRaindrop() {
-    const newDrop = new Raindrop(this.grid, this.mouse);
+    const newDrop = new Raindrop(this.grid, this.mouse, this.light);
     this.raindrops.push(newDrop);
   }
 
   /**
    * Generates splashes and pushes them to the splashes array.
    *
-   * @param { number } dropX
-   * @param { number } dropY
-   * @param { number } size
+   * @param { number } dropX  The x-coordinate of the raindrop.
+   * @param { number } dropY  The y-coordinate of the raindrop.
+   * @param { number } size  The size of the splash.
    *
    * @todo This method is not used. Remove it or use it in the future.
    */
@@ -41,7 +46,6 @@ export class RainController {
     const numSplashes =
       Math.floor(Math.random() * Utils.CONSTANTS.MAX_SPLASHES) +
       Utils.CONSTANTS.MIN_SPLASHES;
-
     for (let i = 0; i < numSplashes; i++) {
       this.splashes.push(new Splash(this.grid, this.mouse, dropX, dropY));
     }
@@ -50,7 +54,7 @@ export class RainController {
   /**
    * Handles the collision between a raindrop and the ground.
    *
-   * @param { Raindrop } drop
+   * @param { Raindrop } drop The raindrop that collided with the ground.
    */
   handleSplashCollision(drop) {
     const splashSize = drop.w;
@@ -120,5 +124,18 @@ export class RainController {
     this.generateRaindrop();
     this.moveRaindrops();
     this.moveSplashes();
+  }
+
+  /**
+   * Render the raindrops and splashes without moving them.
+   * This is used when the animation is paused.
+   */
+  render() {
+    for (const drop of this.raindrops) {
+      drop.render();
+    }
+    for (const splash of this.splashes) {
+      splash.render();
+    }
   }
 }
